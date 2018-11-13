731b7d58e4de8408204380d199c263d685e69a3f
Curiosity48 <43852643+Curiosity48@users.noreply.github.com>
2018-11-13 12:57:53 +0100

    ver 1.3

--- Rover/CMappa.cs
+++ Rover/CMappa.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -12,19 +12,28 @@ namespace Rover
     class CMappa
     {
         //Attributi
-        //Punto di partenza del rover 
+        //Punto contentente la posizione del rover 
         private Point pStart;
         //Vettori per la memorizzazione delle distanze
-        private int[] distanzaDx, distanzaSx;
+        private Point[] pDx, pSx;
         private int numEl;
         //Metodi
         public CMappa()
         {
+            numEl = 0;
             pStart = new Point(0, 0);
         }
+        //Permette di aggiungere due nuovi punti date la distanza da Dx e da Sx
         public void add(int distanzaDx, int distanzaSx)
         {
-            
+            pDx[numEl] = new Point(-distanzaDx, 0);
+            pSx[numEl] = new Point(distanzaSx, 0);
+            numEl++; //Viene incrementato il numero di punti salvati
+        }
+        //Metodo che permetto di variare gli attributi nel momento in cui il drone si muove 
+        public void spostamentoRover(int distanza , int grado, int spostamento)
+        {
+            //TO DO
         }
 
     }
